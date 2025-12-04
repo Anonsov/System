e3ce6c73c6a1a2c083fc58ca655968cec9dfa0eb
Akobir Salomov <akobirsalomov48@gmail.com>
2025-12-04 18:32:10 +0500

    added solved_count column for each user

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -4,5 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    score = models.IntegerField(default=0)   
-    # problems_solved = 
+    score = models.IntegerField(default=0)  
+    solved_count = models.IntegerField(default=0)
+    
+    
