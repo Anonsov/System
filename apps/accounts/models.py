8a01e20c9403e8962d3cb3a54cfcb9711572e414
Akobir Salomov <akobirsalomov48@gmail.com>
2025-12-04 14:41:40 +0500

    Scores added

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,4 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)   
+    # problems_solved = 
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
+from django.views.generic import ListView
 from django.contrib.auth.views import LoginView 
 from django.urls import reverse_lazy
 from .forms import LoginUserForm, RegisterUserForm
 from django.contrib.auth.views import LoginView, LogoutView
+from .models import Profile
 
 
 # Create your views here.
@@ -24,3 +26,8 @@ class RegisterUser(CreateView):
 
 class LogoutUser(LogoutView):
     next_page = '/'
+    
+    
+class ProfileView(ListView):
+    model = Profile
+    template_name = "accounts/profile.html"
