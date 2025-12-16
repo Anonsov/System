9c1870e4870332297e34f85fce011e74019f11a1
Akobir Salomov <akobirsalomov48@gmail.com>
2025-12-16 10:59:40 +0500

    cleaning

--- media/checker_1JUWbGC.py
+++
@@ -1,9 +0,0 @@
-def check(user_output: str, input_data: str) -> bool:
-    x = float(input_data.strip())
-    try:
-        y = float(user_output.strip()) 
-    except ValueError:
-        return False
-    
-    e = 1e-6
-    return abs(y - x) <= e
