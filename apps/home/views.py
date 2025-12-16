c20f292f7354f9e151ad332e5f79d2d972e8622c
Akobir Salomov <akobirsalomov48@gmail.com>
2025-12-16 10:50:51 +0500

    Minor changes

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from apps.accounts.models import Profile
+from apps.problems.models import Problem
+
 # Create your views here.
 
 class HomePageView(TemplateView):
     template_name = 'home/home.html'
-    
-    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['leaderboard'] = Profile.objects.order_by('-score', '-solved_count')[:5]
+        context['featured_problems'] = Problem.objects.order_by('-created_at')[:3]
+        return context
